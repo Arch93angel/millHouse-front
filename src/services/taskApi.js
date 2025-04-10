35b4310b13f706e80494759d8f48bc142fd21190
Dashe Nungnaan Fate <nungnaan.dashe@st.futminna.edu.ng>
2025-04-10 03:32:30 +0100

    Update taskApi.js

--- src/services/taskApi.js
+++ src/services/taskApi.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: 'http://localhost:8000/api/',
+  baseUrl: REACT_APP_API_URL,
   prepareHeaders: (headers, { getState }) => {
       const token = getState().auth.token;
       if (token) {
