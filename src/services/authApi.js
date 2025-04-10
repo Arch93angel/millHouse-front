bd91305f3f299308951fb1e056003112be383c43
Dashe Nungnaan Fate <nungnaan.dashe@st.futminna.edu.ng>
2025-04-10 03:33:11 +0100

    Update authApi.js

--- src/services/authApi.js
+++ src/services/authApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const authApi = createApi({
   reducerPath: 'authApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8000/api/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: REACT_APP_API_URL }),
   endpoints: (builder) => ({
     login: builder.mutation({
       query: (credentials) => ({
