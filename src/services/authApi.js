9eada6546d42c40d6e1cc1a731eb33cdd4498678
Dashe Nungnaan Fate <nungnaan.dashe@st.futminna.edu.ng>
2025-04-11 01:38:07 +0100

    Update authApi.js

--- src/services/authApi.js
+++ src/services/authApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const authApi = createApi({
   reducerPath: 'authApi',
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_API_URL }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://fatedashe.pythonanywhere.com/api/' }),
   endpoints: (builder) => ({
     login: builder.mutation({
       query: (credentials) => ({
