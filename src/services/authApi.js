144330636653e2bf7607444f4d0a5a9001729b62
Dashe Nungnaan Fate <nungnaan.dashe@st.futminna.edu.ng>
2025-04-11 02:16:00 +0100

    update env url

--- src/services/authApi.js
+++ src/services/authApi.js
@@ -1,9 +1,11 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 
+const apiUrl = import.meta.env.VITE_API_URL;
+
 export const authApi = createApi({
   reducerPath: 'authApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://fatedashe.pythonanywhere.com/api/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: apiUrl }),
   endpoints: (builder) => ({
     login: builder.mutation({
       query: (credentials) => ({
--- src/services/taskApi.js
+++ src/services/taskApi.js
@@ -1,7 +1,8 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
+
 const baseQuery = fetchBaseQuery({
-  baseUrl: process.env.REACT_APP_API_URL,
+  baseUrl: import.meta.env.VITE_API_URL,
   prepareHeaders: (headers, { getState }) => {
       const token = getState().auth.token;
       if (token) {
